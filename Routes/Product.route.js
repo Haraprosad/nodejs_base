e9eb8f41355ea3a32162f28716b530f0089beb80
Haraprosad <haraprosadbiswas123@gmail.com>
2024-03-21 01:44:06 +0600

    get and delete api functionalities added

--- Routes/Product.route.js
+++ Routes/Product.route.js
@@ -2,36 +2,48 @@ const express = require("express");
 const router = express.Router();
 const Product = require("../Models/Product.model");
 
-router.get("/", (req, res, next) => {
-  res.send("getting a list of all products.");
+router.get("/", async (req, res, next) => {
+  try {
+    // const result = await Product.find({},{__v:0,name:1,price:1, _id:0});
+    const result = await Product.find({}, { __v: 0 });
+    res.send(result);
+  } catch (error) {
+    console.log(error.message);
+  }
 });
 
-router.post("/", (req, res, next) => {
-  const product = new Product({
-    name: req.body.name,
-    price: req.body.price,
-  });
-  product
-    .save()
-    .then((result) => {
-      console.log(result);
-      res.send(result);
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
+router.post("/", async (req, res, next) => {
+  try {
+    const product = new Product(req.body);
+    const result = await product.save();
+    res.send(result);
+  } catch (error) {
+    console.log(error.message);
+  }
 });
 
-router.get("/:id", (req, res, next) => {
-  res.send("getting a single product");
+router.get("/:id", async (req, res, next) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    res.send(product);
+  } catch (error) {
+    console.log(error.message);
+  }
 });
 
 router.patch("/:id", (req, res, next) => {
   res.send("updating a product");
 });
 
-router.delete("/:id", (req, res, next) => {
-  res.send("deleting a product");
+router.delete("/:id", async(req, res, next) => {
+    const id = req.params.id;
+    try{
+        const result = await Product.findByIdAndDelete(id);
+        res.send(result);
+    }
+    catch(error){
+        console.log(error.message);
+    }
 });
 
 module.exports = router;
